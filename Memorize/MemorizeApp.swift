4cee923ac6cfae59c79f141e5ffda5af39fc5aae
jreiscrf <jonathan.reis.torres@gmail.com>
2023-09-16 20:40:51 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jonathan Reis on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
