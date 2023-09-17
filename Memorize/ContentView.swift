b0906419a8ded63185e0996e7c3eca481fffd01d
jreiscrf <jonathan.reis.torres@gmail.com>
2023-09-16 22:20:09 -0300

    Card jogo da memoria

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,17 +7,48 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CardView: View {
+    var isFaceUp: Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            }
         }
-        .padding()
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+            HStack {
+                ForEach(0..<4) { index in
+                    CardView(isFaceUp: true)
+                }
+            }
+                .padding()
+                .foregroundColor(Color.orange)
+                .font(Font.largeTitle)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
