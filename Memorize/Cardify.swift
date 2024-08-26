fa2e3c72929b1570c6611afe5afdc3203dca61b1
jreiscrf <jonathan.reis.torres@gmail.com>
2024-08-26 12:11:56 -0300

    feat: ViewBuilder + Shape + ViewModifier

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,33 @@
+//
+//  Cardfy.swift
+//  Memorize
+//
+//  Created by Jonathan Reis on 26/08/24.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                content
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.orange)
+            }
+        }
+    }
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -31,27 +31,25 @@ struct CardView: View {
         }
     }
     
+    @ViewBuilder
     func body(for size: CGSize) -> some View {
-        ZStack {
-            if card.isFaceUp {
-                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+        if card.isFaceUp || !card.isMatched {
+            ZStack {
+                Pie(startAngle: Angle.degrees(0 - 90), endAngle: Angle.degrees(110 - 90),
+                    clockwise: true)
+                .padding(5).opacity(0.4)
                 Text(card.content)
-            } else {
-                if !card.isMatched {
-                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.orange)
-                }
+                    .font(Font.system(size: fontSize(for: size)))
+                
             }
-        }.font(Font.system(size: fontSize(for: size)))
+            .cardify(isFaceUp: card.isFaceUp)
+        }
     }
     
     //MARK: - Drawing Constants
-    
-    let cornerRadius: CGFloat = 10.0
-    let edgeLineWidth: CGFloat = 3
 
     func fontSize(for size: CGSize) -> CGFloat {
-        min(size.width, size.height) * 0.75
+        min(size.width, size.height) * 0.7
     }
 }
 
